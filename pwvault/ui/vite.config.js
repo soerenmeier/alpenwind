c64a43cf0d6bf9fdcb3bf0171e4fb981aa2e7221
Sören Meier <soeren@s-me.ch>
2023-08-28 01:05:45 +0200

    [pwvault] add sass

--- pwvault/ui/vite.config.js
+++ pwvault/ui/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import esbuild from 'esbuild';
 import { randomToken } from 'fire/util.js';
+import sveltePreprocess from 'svelte-preprocess';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,7 +20,9 @@ export default defineConfig({
 		}
 	},
 	plugins: [
-		svelte(),
+		svelte({
+			preprocess: sveltePreprocess()
+		}),
 		{
 			name: 'minify',
 			closeBundle: async () => {
