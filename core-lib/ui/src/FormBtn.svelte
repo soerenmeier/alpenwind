b4b71c9f5ad927fc4a482ad125874deabee58ff0
Sören Meier <soeren@s-me.ch>
2024-06-23 00:39:10 +0200

    [core-lib-ui] rename BlueFormBtn to FormBtn

--- core-lib/ui/src/FormBtn.svelte
+++ core-lib/ui/src/FormBtn.svelte
@@ -1,16 +1,16 @@
 <script>
 	export let text = '';
 	export let loading = false;
+	export let color = 'blue';
 </script>
 
-<button on:click class:loading>
+<button on:click class:loading class="color-{color}">
 	<span class="text">{text}</span>
 </button>
 
 <style>
 	button {
 		position: relative;
-		background-color: #3c7bd5;
 		border: none;
 		padding: 8px 18px;
 		color: var(--white);
@@ -19,8 +19,20 @@
 		transition: background-color 0.2s ease;
 	}
 
-	button:hover {
+	.color-blue {
+		background-color: #3c7bd5;
+	}
+
+	.color-blue:hover {
 		background-color: #2465c1;
+	}
+
+	.color-red {
+		background-color: var(--red);
+	}
+
+	.color-red:hover {
+		background-color: var(--dark-red);
 	}
 
 	.loading .text {
